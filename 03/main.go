999916b1e8f662f503121548e6c286091af7a0a7
burak.karaoglan <burakkaraoglan@icloud.com>
2018-11-27 00:25:49 +0300

    functions in template

--- 03/main.go
+++ 03/main.go
@@ -3,13 +3,44 @@ package main
 import (
 	"log"
 	"os"
+	"path"
+	"strings"
 	"text/template"
 )
 
 var tpl *template.Template
 
+type sage struct {
+	Name  string
+	Motto string
+}
+
+type car struct {
+	Manufacturer string
+	Model        string
+	Doors        int
+}
+
+// create a FuncMap to register functions.
+// "uc" is what the func will be called in the template
+// "uc" is the ToUpper func from package strings
+// "ft" is a func I declared
+// "ft" slices a string, returning the first three characters
+var fm = template.FuncMap{
+	"uc": strings.ToUpper,
+	"ft": firstThree,
+}
+
+func firstThree(s string) string {
+	s = strings.TrimSpace(s)
+	if len(s) >= 3 {
+		s = s[:3]
+	}
+	return s
+}
+
 func init()  {
-	tpl = template.Must(template.ParseFiles("03/tpl.gohtml"))
+	tpl = template.Must(template.New(path.Base("03/tpl.gohtml")).Funcs(fm).ParseFiles("03/tpl.gohtml"))
 }
 
 func main() {
